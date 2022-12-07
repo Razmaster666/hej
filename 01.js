ecfb577633160813116f437bde0dcdb17a2382a8
Razmaster666 <113451916+Razmaster666@users.noreply.github.com>
2022-12-07 10:06:57 +0100

    Update 01.js

--- 01.js
+++ 01.js
@@ -12,8 +12,11 @@ var currentTab = 0;
 
 showTab(currentTab);
 
-function goToNextQ() {
+const finalVillain = document.getElementById("finalVillain");
 
+
+function goToNextQ() {
+    
     const villain = document.querySelectorAll("input:checked")[next].value
     if (villain == "Magneto") {
         skurkar.magneto++
@@ -23,16 +26,16 @@ function goToNextQ() {
         joker++
     }
     next++
-
+    
     console.log(villain)
-
+    
     console.log(`Joker: ${skurkar.joker}, Doom: ${skurkar.doom}`)
     console.log("Joker: " + joker)
     console.log("Doom: " + drDoom)
     console.log("Magneto: " + magneto)
-
+    
     nextPrev()
-
+    
 }
 
 function showTab(n) {
@@ -49,37 +52,37 @@ function nextPrev(n) {
 
 }
 
-function hideButton(button){
+function hideButton(button) {
     button.style.display = "none";
 }
 
-function showButton(button){
+function showButton(button) {
     button.style.display = "block";
 }
 
 function result() {
-
-    let results = 0;
-
+    
+    finalVillain.src = "kraven.jpg"
+    
+    /*     let results = 0;
+    
     for (let i = 0; i <= drDoom || i <= joker || i <= magneto; i++) {
         results = i;
     }
-
+    
     if (results == drDoom) {
         console.log("Doom wins")
-        banan.innerHTML = "Doom wins";
-    
+        banan.innerHTML = "Doom wins";        
     }
-
+    
     if (results == joker) {
         console.log("Joker wins")
         banan.innerHTML = "Joker wins";
-      
     }
 
     if (results == magneto) {
         console.log("Magneto wins")
         banan.innerHTML = "Magneto wins"
-    }
+    } */
 }
 

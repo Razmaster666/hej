9eb7cf260ee6df14d56fc8bb863575be4fc3b9f8
Razmaster666 <113451916+Razmaster666@users.noreply.github.com>
2022-12-07 11:05:52 +0100

    Update 01.js

--- 01.js
+++ 01.js
@@ -1,12 +1,12 @@
 let drDoom = 0;
-let magneto = 0;
-let joker = 0;
+let harley = 0;
+let jabba = 0;
+let jafar = 0;
+
 var next = 0;
 
 const banan = document.getElementById("finalResult");
 const banan2 = document.getElementsByClassName("cool:hover")
-
-const skurkar = { joker: 0, doom: 0, magneto: 0 }
 
 var currentTab = 0;
 
@@ -16,26 +16,30 @@ const finalVillain = document.getElementById("finalVillain");
 
 
 function goToNextQ() {
-    
+
     const villain = document.querySelectorAll("input:checked")[next].value
-    if (villain == "Magneto") {
-        skurkar.magneto++
-    } else if (villain == "Doom") {
+
+    if (villain == "Doom") {
         drDoom++
-    } else if (villain == "Joker") {
-        joker++
+    } else if (villain == "Quinn") {
+        harley++
+    } else if (villain == "Jafar") {
+        jafar++
+    } else if (villain == "Hutt") {
+        jabba++
     }
+
     next++
-    
-    console.log(villain)
-    
-    console.log(`Joker: ${skurkar.joker}, Doom: ${skurkar.doom}`)
-    console.log("Joker: " + joker)
+
+    console.log(villain + " gets one point")
+
+    console.log("Jafar: " + jafar)
     console.log("Doom: " + drDoom)
-    console.log("Magneto: " + magneto)
-    
+    console.log("Hutt: " + jabba)
+    console.log("Quinn: " + harley)
+
     nextPrev()
-    
+
 }
 
 function showTab(n) {
@@ -49,7 +53,6 @@ function nextPrev(n) {
     x[currentTab].style.display = "none";
     currentTab += 1
     showTab(currentTab);
-
 }
 
 function hideButton(button) {
@@ -61,28 +64,33 @@ function showButton(button) {
 }
 
 function result() {
+
+  /*   finalVillain.src = "kraven.jpg" */
+
+      let results = 0;
     
-    finalVillain.src = "kraven.jpg"
-    
-    /*     let results = 0;
-    
-    for (let i = 0; i <= drDoom || i <= joker || i <= magneto; i++) {
+    for (let i = 0; i <= drDoom || i <= harley || i <= jabba || i <= jafar; i++) {
         results = i;
     }
     
     if (results == drDoom) {
         console.log("Doom wins")
-        banan.innerHTML = "Doom wins";        
+        banan.innerHTML = "You are Dr Doom! Your intellect is superior to that of others, but it comes with a backside of vanity. You possess an iron will and are fit to lead!";        
     }
     
-    if (results == joker) {
-        console.log("Joker wins")
-        banan.innerHTML = "Joker wins";
+    if (results == harley) {
+        console.log("Harley wins")
+        banan.innerHTML = "You are Harley Quinn! You are bubbly, energetic and charming. Chaos tends to follow you wherever you go. You have an easy time falling in love, but be careful as the love might consume you!";
     }
 
-    if (results == magneto) {
-        console.log("Magneto wins")
-        banan.innerHTML = "Magneto wins"
-    } */
+    if (results == jafar) {
+        console.log("Jafar wins")
+        banan.innerHTML = "You are Jafar! A master of manipulation and the mystic arts, you are a formidable person in every respect. You have a high opinion of yourself, perhaps a bit unearned. Your smile lights up every room."
+    }
+
+    if (results == jabba) {
+        console.log("Jabba wins")
+        banan.innerHTML = "You are Jabba the Hutt! Your love of money and power knows no bounds, and you are prone to give into both lust and gluttony. The friends you keep are a ragged bunch. You are easily distracted by girls in bikinis."
+    }
 }
 
